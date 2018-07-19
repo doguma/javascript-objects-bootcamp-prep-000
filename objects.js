38ecb17dec21a1566b1c2fbed73f4104a66ab633
Hannah Do <hannah.hj.do@gmail.com>
2018-07-19 00:29:53 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { songName: 'this is america', artist: 'childish gambino'};
+
+function updatePlaylist(playlist, songName, artist){
+  playlist
+  return playlist;
+}
+
+function removeFromPlayList(playlist, artist){
+  delete playlist.artist;
+  return playlist;
+}
